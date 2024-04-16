0b37926506d5c322602de1fcc4645c728a17f8b7
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-16 17:54:28 +0300

    Добавлен компонент таблицы и запрос к бд для вывода заявок

--- src/pages/RequestsPage/RequestsPage.jsx
+++ src/pages/RequestsPage/RequestsPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Container } from '@mui/material';
+import RequestsTable from '@components/RequestsTable/RequestsTable';
+import { getUserRequests } from '@services/api.js';
 
 export default function RequestsPage() {
+  getUserRequests()
+    .then(res => console.log(res));
+    
   return (
-    <div>RequestsPage</div>
+    <Container>
+      <RequestsTable/>
+    </Container>
   )
 }
