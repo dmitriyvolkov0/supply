79869546c4b14856ebcf8b6a2260dd2171936928
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-17 17:32:24 +0300

    Добавлены функции для взаимодейтсвия с заявкми в зависимости от роли пользователя

--- src/components/RequestsTable/Row/Tools/Tools.jsx
+++ src/components/RequestsTable/Row/Tools/Tools.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext }  from 'react';
+import UserContext from '@contexts/User/UserContext';
+import ActionsContext from '@contexts/Actions/ActionsContext';
 import { useNavigate } from 'react-router-dom';
 
 import Tooltip from '@mui/material/Tooltip';
@@ -15,8 +17,151 @@ import WarehouseIcon from '@mui/icons-material/Warehouse';
 import FactoryIcon from '@mui/icons-material/Factory';
 import HistoryIcon from '@mui/icons-material/History';
 
+const ButtonSkeleton = ({title, icon, onClick}) => {
+    return <Tooltip title={title}>
+                <IconButton 
+                    onClick={onClick}
+                    aria-label={title}
+                >
+                    {icon}
+                </IconButton>
+            </Tooltip>
+}
 
-export default function Tools({ isOpen, setOpen }) {
+const EditBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Редактировать запись" icon={<EditIcon />}/>
+}
+
+const DeleteBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Удалить запись" icon={<DeleteIcon />}/>
+}
+
+const InArchiveBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Закрыть и перенести в архив" icon={<DoneAllIcon />}/>
+}
+
+// Принять в обработку (склад)
+const HandleRequestWarehouseBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Принять в обработку" icon={<AppRegistrationIcon />}/>
+}
+
+const IndicateBalancesBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Указать остатки" icon={<DoneAllIcon />}/>
+} 
+
+const ArrivedInWarehouseBut = ({onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Материалы готовы к выдаче" icon={<DoneAllIcon />}/>
+} 
+
+const MaterialTransferredBut  = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Материал выдан/получен" icon={<DoneAllIcon />}/>
+} 
+// ================
+
+// Принять в обработку (снабжение)
+const HandleRequestSnabBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Принять в обработку" icon={<AppRegistrationIcon />}/>
+}
+
+//Указать,что материалы прибыли на склад
+const MaterialsArrivedWarehouseBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Материалы прибыли на склад" icon={<WarehouseIcon />}/>
+}
+
+//Указать,что материалы прибыли на объект
+const MaterialsArrivedObjectBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Материалы прибыли на объект" icon={<FactoryIcon />}/>
+}
+
+// Принять в обработку (контроль)
+const HandleRequestControlBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Принять в обработку" icon={<AppRegistrationIcon />}/>
+}
+
+//Информация была исправлена контролёром
+const MaterialsEditControlBut = ({ onClick }) => {
+    return <ButtonSkeleton onClick={onClick} title="Подтвердить и отправить далее" icon={<DoneAllIcon />}/>
+}
+
+
+// Кнопки заказчика
+const UserButtons = ({requestId, statusId}) => {
+    const { actions } = useContext(ActionsContext);
+    switch(statusId){// Выводим кнопки в зависимости от статуса заявки
+        case 1:
+            return <>
+                <DeleteBut onClick={() => actions.deleteRequest(requestId)}/>
+                <EditBut onClick={() => actions.editRequest(requestId)}/>
+            </>
+        case 10:
+            return +statusId !== 11 && <InArchiveBut onClick={() => actions.inArchive(requestId)}/>
+        default:
+            break;
+    }
+}
+
+// Кнопки зав.складом
+const WarehouseButtons = ({blankId, statusId}) => {
+    const { actions } = useContext(ActionsContext);
+    switch (statusId) {
+        case 1:
+            return <HandleRequestWarehouseBut onClick={() => actions.handleRequestWarehouse(blankId)}/>
+        case 2:
+            return <IndicateBalancesBut onClick={() => actions.indicateBalances(blankId)}/>
+        case 7:
+            return <ArrivedInWarehouseBut onClick={() => actions.arrivedInWarehouse(blankId)}/>
+        case 8:
+            return <MaterialTransferredBut onClick={() => actions.materialTransferred(blankId)}/>
+        case 9:
+            return <MaterialTransferredBut onClick={() => actions.materialTransferred(blankId)}/>
+        default:
+            break;
+    }
+}
+
+// Кнопки снабжения
+const SnabButtons = ({ blankId, statusId }) => {
+    const { actions } = useContext(ActionsContext);
+    switch (statusId) {
+        case 5:
+            return <>
+                <EditBut onClick={() => actions.editRequest(blankId)}/> 
+                <HandleRequestSnabBut onClick={() => actions.handleRequestSnab(blankId)}/>
+            </>;
+        case 6:
+            return <>
+                <EditBut onClick={() => actions.editRequest(blankId)}/> 
+                <MaterialsArrivedWarehouseBut onClick={() => actions.materialsArrivedWarehouse(blankId)}/>
+                <MaterialsArrivedObjectBut onClick={() => actions.materialsArrivedObject(blankId)}/>
+            </>
+    
+        default:
+            break;
+    }
+}
+
+// Кнопки контролёра
+const ControlButtons = ({ blankId, statusId }) => {
+    const { actions } = useContext(ActionsContext);
+    switch (statusId) {
+        case 3:
+            return <>
+                <HandleRequestControlBut onClick={() => actions.handleRequestControlBut(blankId)}/>
+            </>;
+        case 4:
+            return <>
+                <EditBut onClick={() => actions.editRequest(blankId)}/> 
+                <MaterialsEditControlBut onClick={() => actions.editRequest(blankId)}/> 
+            </>
+    
+        default:
+            break;
+    }
+}
+
+
+export default function Tools({ data, isOpen, setOpen, hideButtons }) {
+    const { user } = useContext(UserContext);
     const navigate = useNavigate();
     return (
         <>
@@ -37,6 +182,27 @@ export default function Tools({ isOpen, setOpen }) {
                     <HistoryIcon/>
                 </IconButton>
             </Tooltip>   
+
+            {!hideButtons && 
+                <>
+                    {+user.role_id === 2 &&
+                        <UserButtons requestId={+data.id} statusId={+data.status_id}/>
+                    }
+        
+                    {+user.role_id === 3 &&
+                        <WarehouseButtons requestId={+data.id} statusId={+data.status_id}/>
+                    }
+        
+                    {+user.role_id === 4 &&
+                        <SnabButtons requestId={+data.id} statusId={+data.status_id}/>
+                    }
+        
+                    {+user.role_id === 5 &&
+                        <ControlButtons requestId={+data.id} statusId={+data.status_id}/>
+                    }
+                
+                </>
+            }
         </>
     )
 }
