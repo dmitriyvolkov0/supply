f71576295c60392468f68da82c03c71ad8ca4ea0
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-17 12:29:28 +0300

    Реализован вывод заявок

--- src/components/RequestsTable/Row/Row.jsx
+++ src/components/RequestsTable/Row/Row.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
+import s from './style.module.css';
+
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
-import { Box } from '@mui/material';
 
-export default function Row() {
-  return (
-    <>
-        <TableRow>
-            <TableCell>
-                <Box className='flex justify-start items-center'>
-                    {/* <Tools 
-                        data={data} 
-                        isOpen={isOpen} 
-                        setOpen={setOpen}
-                        hideButtons={hideButtons}
-                    /> */}
-                </Box>
+import Tools from './Tools/Tools';
+import Status from './Status/Status';
+import SubTable from './SubTable/SubTable';
 
-            </TableCell>
-            <TableCell align="right">1</TableCell>
-            <TableCell align="right">Склад</TableCell>
-            <TableCell align="right">Иванов Иван Иванович</TableCell>
-            <TableCell align="right">Объект</TableCell>
-            <TableCell align="right">24.02.2022 16:32</TableCell>
-            <TableCell align="center">
-                Статус
-                {/* <Status statusId={data.status_id} statusText={data.status_name} warehouseName={data.warehouse_name}/> */}
-            </TableCell>
-        </TableRow>   
-    </>
-  )
+import { getFullDateByStrFromDb } from '@utils/helpers/timeFunctions.js';
+
+export default function Row({ data, hideButtons }) {
+    const [isOpen, setOpen] = React.useState(false);
+    return (
+        <>
+            <TableRow>
+                <TableCell>
+                    <div className={s.tools}>
+                        <Tools 
+                            data={data} 
+                            isOpen={isOpen} 
+                            setOpen={setOpen}
+                            hideButtons={hideButtons}
+                        />
+                    </div>
+
+                </TableCell>
+                <TableCell align="right">{data.id}</TableCell>
+                <TableCell align="right">{data.division_name}</TableCell>
+                <TableCell align="right">{data.user_name}</TableCell>
+                <TableCell align="right">{data.text}</TableCell>
+                <TableCell align="right">{getFullDateByStrFromDb(data.created_at)}</TableCell>
+                <TableCell align="center">
+                    <Status statusId={data.status_id} statusName={data.status_name} warehouseName={data.warehouse_name}/>
+                </TableCell>
+            </TableRow>   
+
+            <SubTable requestId={data.id} isOpen={isOpen}/>
+        </>
+    )
 }
