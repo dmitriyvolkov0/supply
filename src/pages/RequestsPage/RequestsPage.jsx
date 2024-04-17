eb3453149fee8518b3d5769c5786a8076de42110
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-17 12:29:43 +0300

    Реализован вывод заявок

--- src/pages/RequestsPage/RequestsPage.jsx
+++ src/pages/RequestsPage/RequestsPage.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import RequestsTable from '@components/RequestsTable/RequestsTable';
 import { getUserRequests } from '@services/api.js';
 
-export default function RequestsPage() {
-  getUserRequests()
-    .then(res => console.log(res));
-    
+export default function RequestsPage({ user }) {
+  const [requests, setRequests] = useState(null);
+  const [perPage, setPerPage] = useState(10);
+  
+  useEffect(() => {
+    let status = 'active';
+    getUserRequests(`requests?status=${status}&userId=${user.id}&count=${perPage}`)
+      .then(res => setRequests(res));
+  }, []);
+
   return (
     <Container>
-      <RequestsTable/>
+      <RequestsTable requests={requests}/>
     </Container>
   )
 }
