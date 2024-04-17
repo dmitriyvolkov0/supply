a54f8c3901c3f88d2e981b58939db911cc5a77eb
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-17 15:16:08 +0300

    Добавлен контекст для взаимодействия пользователя с заявками

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import UserContextProvider from './contexts/User/UserContextProvider';
+import ActionsContextProvider from './contexts/Actions/ActionsContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <ActionsContextProvider>
+        <App />
+      </ActionsContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 );
