1b3ebfd186208a6c8a48c835e596e07fdf150c0b
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-17 14:55:15 +0300

    Настроено оформление статуса

--- src/components/RequestsTable/Row/Status/Status.jsx
+++ src/components/RequestsTable/Row/Status/Status.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import s from './style.module.css';
 
-export default function Status({statusId, statusName}) {
-  return (
-    <div>{statusName}</div>
-  )
+export default function Status({statusId, statusName, warehouseName}) {
+    let colorClass;
+    switch (+statusId) {
+        case 2:
+            colorClass = s.cl_2;
+            break;
+        case 3:
+            colorClass = s.cl_3;
+            break;
+        case 4:
+            colorClass = s.cl_4;
+            break;
+        case 5:
+            colorClass = s.cl_5;
+            break;
+        case 6:
+            colorClass = s.cl_6;
+            break;
+        case 7:
+            colorClass = s.cl_7;
+            break;
+        case 8:
+            colorClass = s.cl_8;
+            break;
+        case 9:
+            colorClass = s.cl_9;
+            break;
+        case 10:
+            colorClass = s.cl_10;
+            break;
+        case 11:
+            colorClass = s.cl_11;
+            break;
+        default:
+            colorClass = s.cl_1;
+    }
+    
+    return (
+        <div className={`${colorClass} ${s.status}`}>
+            {statusName}
+            
+            {+statusId === 6 && warehouseName && 
+                <p className={s.warehouseText}>
+                    {warehouseName}
+                </p>
+            }
+        </div>
+    )
 }
