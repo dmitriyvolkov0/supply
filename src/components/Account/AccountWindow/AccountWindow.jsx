1500445ddc2d8bf2fc77c998c0716ce83018f08a
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-18 17:55:42 +0300

    Добавлен вывод отдела, к которому принадлежит пользователь и его роль

--- src/components/Account/AccountWindow/AccountWindow.jsx
+++ src/components/Account/AccountWindow/AccountWindow.jsx
@@ -3,7 +3,7 @@ import s from './style.module.css';
 
 import Avatar from '@mui/material/Avatar';
 import { blue } from '@mui/material/colors';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Tooltip } from '@mui/material';
 
 export default function AccountWindow({user, setUser, accountWindowWrapperRef, firstLetters}) {
     const logout = () => {
@@ -11,17 +11,23 @@ export default function AccountWindow({user, setUser, accountWindowWrapperRef, f
         setUser(false);
     }
 
+    console.log(user);
+
     return (
         <div ref={accountWindowWrapperRef} className={s.accountDataWrapper}>
             <div className={s.accountData}>
+                <p className={s.userDivision}>
+                    {user.division_name}
+                </p>
                 <Avatar sx={{ bgcolor: blue[400], fontSize: 24, width: 55, height: 55 }}>
                     {firstLetters}
                 </Avatar>
                 <Typography className={s.userName} variant="p" component="p">{user.name}</Typography>
                 <p className={s.userEmail}>{user.email}</p>
-                <p className={s.userDivision}>
-                    Отдел...
-                </p>
+                <Tooltip title="Ваша роль">
+                    <p className={s.userRole}>{user.role_name}</p>
+                </Tooltip>
+
                 <Button onClick={logout} className={s.logoutBut} variant='outlined'>Выйти</Button>
             </div>
         </div>
