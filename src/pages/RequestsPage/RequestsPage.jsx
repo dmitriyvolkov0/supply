a210c59a2fb6e1c593ca99ee49562cc02e36eb0e
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-18 12:58:30 +0300

    Добавлена возможность указания остатков на складе

--- src/pages/RequestsPage/RequestsPage.jsx
+++ src/pages/RequestsPage/RequestsPage.jsx
@@ -3,8 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import ActionsContext from '@contexts/Actions/ActionsContext';
 
 import { Container } from '@mui/material';
+import { 
+  getUserRequests, 
+  deleteRequestById, 
+  setStatus, 
+  getMaterialsByRequestId, 
+  setBalances 
+} from '@services/api.js';
+
 import RequestsTable from '@components/RequestsTable/RequestsTable';
-import { getUserRequests, deleteRequestById, setStatus } from '@services/api.js';
+import IndicateBalancesModal from '@widgets/IndicateBalancesModal/IndicateBalancesModal';
 
 export default function RequestsPage({ user }) {
   const { setActions } = useContext(ActionsContext); //контекст с методами изменения состояния заявки
@@ -12,7 +20,12 @@ export default function RequestsPage({ user }) {
 
   const [requests, setRequests] = useState(null);
   const [perPage, setPerPage] = useState(10);
+
+  // modals
+  const [isModalIndicateBalancesOpen, setIsModalIndicateBalancesOpen] = useState(false); 
   
+  const [materials, setMaterials] = useState([]); //Массив материалов, для которых будут указываться остатки
+
   // Получить все заявки, доступные пользователю
   const getAllRequests = (count) => {
     getUserRequests(user.id, count, 'active')
@@ -56,9 +69,44 @@ export default function RequestsPage({ user }) {
       }
     }
 
-    // Указать остатки на складе
+      // Получить список материалов, принадлежащих выбранной заявке для установки остатков
+    const getMaterials = (requestId) => {
+      getMaterialsByRequestId(requestId)
+        .then(res => {
+          setMaterials(res);
+        }).catch(err => alert('Возникла внутренняя ошибка!'));
+    }
+
+    // Указать остатки материалов на складе (открыть модальное окно)
     const indicateBalances = (requestId) => {
-      console.log('Указаны остатки');
+      setIsModalIndicateBalancesOpen(true);
+      getMaterials(requestId);
+    }
+
+    // Установить указанные остатки материалов в бд
+    const setBalancesHandle = (materials) => {
+      let requestId = materials[0].request_id;
+
+      setBalances(materials)
+        .then(res => {
+          if(res.status){
+            alert('Остатки успешно указаны!');
+            setIsModalIndicateBalancesOpen(false);
+          }else{
+            alert('Возникла ошибка при указании остатков!');
+          }
+        })
+
+        .then(() => {
+          setStatus(requestId, 3)
+            .then(res => {
+              if(!res.status){
+
+              }
+            })
+            .catch(err => alert('Возникла внутренняя ошибка!'));
+        })
+        .catch(err => alert('Возникла внутренняя ошибка!'));
     }
 
   useEffect(() => {
@@ -83,6 +131,14 @@ export default function RequestsPage({ user }) {
   return (
     <Container>
       <RequestsTable requests={requests}/>
+      <IndicateBalancesModal
+        title="Укажите остатки материалов"
+        materials={materials}
+        setMaterials={setMaterials}
+        setBalancesHandle={setBalancesHandle}
+        isOpen={isModalIndicateBalancesOpen}
+        setIsOpen={setIsModalIndicateBalancesOpen}
+      />
     </Container>
   )
 }
