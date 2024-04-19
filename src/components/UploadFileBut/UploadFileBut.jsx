485d4182282dbd4a1ff2be6e1b9ea4c18921efcd
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-19 17:49:13 +0300

    Добавлена кнопка загрузки файлов

---
+++ src/components/UploadFileBut/UploadFileBut.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export default function UploadFileBut({ materialIndex, uploadFilesHandle }) {
+    const handleFileUpload = (event) => {
+        let files = Array.from(event.target.files);
+        files = files.map(file => ({
+            name: file.name,
+            type: file.type,
+            url: URL.createObjectURL(file)
+          }));
+        uploadFilesHandle(files, materialIndex);
+    };
+
+    return (
+        <>
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                sx={{marginTop: '10px'}}
+                startIcon={<CloudUploadIcon />}
+                >
+                    Прикрепить файлы
+                    <VisuallyHiddenInput type="file" multiple onChange={handleFileUpload} />
+            </Button>
+        </>
+    );
+}
