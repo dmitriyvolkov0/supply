b37a5dd81c732b444aaaa7ee9688921c68af2eef
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-23 16:36:57 +0300

    Добавлены пути для страницы истории

--- src/utils/constants/routes.js
+++ src/utils/constants/routes.js
@@ -3,4 +3,6 @@ export const CREATE_REQUEST_PAGE = '/create-request';
 export const AUTH_PAGE = '/auth';
 export const ARCHIVE_PAGE = '/archive';
 export const SEARCH_PAGE = '/search';
+export const HISTORY_PAGE = '/history/';
+export const HISTORY_SINGLE_PAGE = '/history/:id';
 export const ADMIN_PAGE = '/admin';
