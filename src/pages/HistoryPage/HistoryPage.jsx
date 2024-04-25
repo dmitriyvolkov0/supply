8585b6a4951b7eb8df66caf0d8f79f966da89579
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 16:02:52 +0300

    Добавлено получение истории

--- src/pages/HistoryPage/HistoryPage.jsx
+++ src/pages/HistoryPage/HistoryPage.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
+import HistoryTable from '@widgets/HistoryTable/HistoryTable';
+import { useParams } from 'react-router-dom';
+import TitleBack from '@components/TitleBack/TitleBack';
+import { REQUESTS_PAGE } from '@utils/constants/routes.js';
+import RefreshBut from '@components/RefreshBut/RefreshBut';
+import Box from '@mui/material/Box';
+
+import { getHistoryByRequestId } from '@services/api.js';
 
 export default function HistoryPage() {
+  const { requestId } = useParams();
+  const [history, setHistory] = useState(null);
+  
+  const getHistoryByRequestIdHandle = () => {
+    getHistoryByRequestId(requestId)
+      .then(res => {
+        if(res.status){
+          setHistory(res.data);
+        }else{
+          setHistory([]);
+          alert('При получении истории заявки возникла ошибка!');
+        }
+      })
+      .catch((err) => alert('Возникла внутренняя ошибка!'));
+  }
+
+  useEffect(() => {
+    getHistoryByRequestIdHandle();
+  }, [])
+  
   return (
     <Container>
-      
+      <Box sx={{display: 'flex', justifyContent:'space-between', alignItems: 'center'}}>
+        <TitleBack title={"Заявка №" + requestId} link={REQUESTS_PAGE}/>
+        <RefreshBut onClick={getHistoryByRequestIdHandle} size={32}/>
+      </Box>
+      <HistoryTable history={history}/>
     </Container>
   )
 }
