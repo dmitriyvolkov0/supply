290b63081e3be43caf401eeb2ab72d2150cc8a66
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 16:38:16 +0300

    Поправлены стили

--- src/widgets/RequestsToolBar/RequestsToolBar.jsx
+++ src/widgets/RequestsToolBar/RequestsToolBar.jsx
@@ -3,16 +3,17 @@ import { Box, Button } from '@mui/material';
 import { CREATE_REQUEST_PAGE } from '@utils/constants/routes';
 import { useNavigate } from 'react-router-dom';
 import RefreshBut from '@components/RefreshBut/RefreshBut';
+import s from './style.module.css';
 
 export default function RequestsToolBar({ getAllRequests }) {
     const navigate = useNavigate();
     return (
-        <Box sx={{display: 'flex', gap: '10px', marginBottom: '15px'}}>
+        <div class={s.wrapper}>
             <Button onClick={() => navigate(CREATE_REQUEST_PAGE)} variant="outlined">Создать заявку</Button>
             <RefreshBut 
                 onClick={getAllRequests}
                 color="primary"    
             />
-        </Box>
+        </div>
     )
 }
