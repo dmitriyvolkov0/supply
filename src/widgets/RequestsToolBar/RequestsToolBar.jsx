c490a18a6885dfa91d468e79634bd227753e7400
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 16:39:05 +0300

    Мелкие правки

--- src/widgets/RequestsToolBar/RequestsToolBar.jsx
+++ src/widgets/RequestsToolBar/RequestsToolBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { CREATE_REQUEST_PAGE } from '@utils/constants/routes';
 import { useNavigate } from 'react-router-dom';
 import RefreshBut from '@components/RefreshBut/RefreshBut';
