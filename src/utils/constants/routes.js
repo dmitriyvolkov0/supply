5c0b95b34f8c96a865305c7eaac53f76830116a3
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 14:47:29 +0300

    Изменен параметр

--- src/utils/constants/routes.js
+++ src/utils/constants/routes.js
@@ -6,5 +6,5 @@ export const AUTH_PAGE = '/auth';
 export const ARCHIVE_PAGE = '/archive';
 export const SEARCH_PAGE = '/search';
 export const HISTORY_PAGE = '/history/';
-export const HISTORY_SINGLE_PAGE = '/history/:id';
+export const HISTORY_SINGLE_PAGE = '/history/:requestId';
 export const ADMIN_PAGE = '/admin';
