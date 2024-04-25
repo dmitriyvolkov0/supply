f78dba4cd4c02dc10c972c47d3a1a320a905b93a
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 17:54:35 +0300

    Реализован вывод архивных заявок

--- src/pages/ArchivePage/ArchivePage.jsx
+++ src/pages/ArchivePage/ArchivePage.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import RequestsTable from '@widgets/RequestsTable/RequestsTable';
+import { Container } from '@mui/material';
 
-export default function ArchivePage() {
+import { getUserRequests } from '@services/api.js';
+
+export default function ArchivePage({user}) {
+  const [requests, setRequests] = useState(null);
+  const [perPage, setPerPage] = useState(10);
+
+  // Получить все архивные заявки, доступные пользователю
+  const getAllRequests = (count) => {
+    getUserRequests(user.id, count, 'archive')
+      .then(res => setRequests(res));
+  }
+
+  useEffect(() => {
+    getAllRequests(perPage);
+    const interval = setInterval(() => {
+      getAllRequests(perPage);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [perPage]);
+
   return (
-    <div>ArchivePage</div>
+    <Container>
+      <RequestsTable requests={requests}/>
+    </Container>
   )
 }
