74f066156e52760dcfc290848f3d8ddabf720c0f
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-25 16:20:56 +0300

    Оформлено уведомление о том, что таблица пустая

--- src/components/EmptyTable/EmptyTable.jsx
+++ src/components/EmptyTable/EmptyTable.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import s from './style.module.css';
+import SummarizeIcon from '@mui/icons-material/Summarize';
 
-export default function EmptyTable() {
+export default function EmptyTable({title="Здесь пока нет записей"}) {
   return (
-    <div>EmptyTable</div>
+    <div className={s.wrapper}>
+        <SummarizeIcon className={s.icon}/>
+        <p className={s.title}>{title}</p>
+    </div>
   )
 }
