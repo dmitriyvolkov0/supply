79304a81601c3e1cc5e9a6381c529a4962fd527a
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-26 12:14:07 +0300

    исправлена ошибка, когда при клике на кнопку `вложения` не открывалось модальное окно с вложениями

--- src/pages/ArchivePage/ArchivePage.jsx
+++ src/pages/ArchivePage/ArchivePage.jsx
@@ -1,18 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import RequestsTable from '@widgets/RequestsTable/RequestsTable';
 import { Container } from '@mui/material';
 import LazyLoadingBut from '@components/LazyLoadingBut/LazyLoadingBut';
-
+import FilesModal from '@widgets/FilesModal/FilesModal';
 import { getUserRequests } from '@services/api.js';
+import ActionsContext from '@contexts/Actions/ActionsContext';
 
 export default function ArchivePage({user}) {
+  const { setActions } = useContext(ActionsContext);
+
   const [requests, setRequests] = useState(null);
   const [perPage, setPerPage] = useState(10);
+
+  const [isModalFilesOpen, setIsModaFilesOpen] = useState(false);
+  const [files, setFiles] = useState(null); //Вложения
 
   // Получить все архивные заявки, доступные пользователю
   const getAllRequests = (count) => {
     getUserRequests(user.id, count, 'archive')
       .then(res => setRequests(res));
+  }
+
+  // Открыть модальное окно просмотра вложений
+  const showFilesModal = (files) => {
+    setIsModaFilesOpen(true);
+    setFiles(files);
   }
 
   useEffect(() => {
@@ -23,13 +35,29 @@ export default function ArchivePage({user}) {
     return () => clearInterval(interval);
   }, [perPage]);
 
+  useEffect(() => {
+    let actions = {
+      showFilesModal: showFilesModal
+    };
+    setActions(actions);
+  }, []);
+
   return (
     <Container>
       <RequestsTable requests={requests}/>
+
       <LazyLoadingBut
         requests={requests}
         perPage={perPage}
-        setPerPage={setPerPage}/>
+        setPerPage={setPerPage}
+      />
+        
+      <FilesModal
+        title="Вложения"
+        files={files}
+        isOpen={isModalFilesOpen}
+        setIsOpen={setIsModaFilesOpen}
+      />
     </Container>
   )
 }
