84c254df759978f88e89f0fdebf02549065ff7a6
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-26 17:59:38 +0300

    В процессе разработки

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import MainLayout from '@layouts/MainLayout';
+import Avatar from '@mui/material/Avatar';
+import { blue } from '@mui/material/colors';
+import { Box, Typography } from '@mui/material';
 
 export default function ProfilePage() {
   return (
     <MainLayout>
-        asasd
+        <Box sx={{display: 'flex', gap: '15px'}}>
+            <Avatar sx={{ bgcolor: blue[400], fontSize: 45, width: 100, height: 100}}>
+                ИС
+            </Avatar>
+            
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <Typography sx={{fontSize: '25px'}}>Иванов Иван Иванович</Typography>
+                <a href="mailto:ivanov@mail.ru">ivanov@mail.ru</a>
+            </Box>
+        </Box>
     </MainLayout>
   )
 }
