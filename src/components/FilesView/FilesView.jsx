aff33f186370a3394e271f33b9fb9fcf92dd99ad
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-27 17:06:15 +0300

    добавлен вывод размера файла, оптимизирован вывод информации

--- src/components/FilesView/FilesView.jsx
+++ src/components/FilesView/FilesView.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import DownloadIcon from '@mui/icons-material/Download';
-import { Box, IconButton, Tooltip } from '@mui/material';
+import { IconButton, Tooltip } from '@mui/material';
+import s from './style.module.css';
+import fileSizeConverter from '@utils/helpers/fileSizeConverter.js';
 
 export default function FilesView({ files }) {
+    
     return (
         <>
             {
                 files && files.length > 0 &&
                     files.map((item, index) => 
-                        <Box key={"file-"+index} sx={{display: 'flex', alignItems: 'center', gap: '10px', width:"350px", maxWidth: '100%'}}>
+                        <div 
+                            key={"file-"+index}
+                            className={s.wrapper}
+                        >
                             <Tooltip title="Скачать файл">
                                 <IconButton href={URL.createObjectURL(item)} download>
                                     <DownloadIcon/>
                                 </IconButton>
                             </Tooltip>
-                            <p>
-                                {item.name}
+
+                            <Tooltip title={item.name}>
+                                <p className={s.fileName}>{item.name}</p>
+                            </Tooltip>
+
+                            <p className={s.fileSize}>
+                                {fileSizeConverter(item.size)}
                             </p>
-                        </Box>
+                        </div>
                     )
             }
         </>
