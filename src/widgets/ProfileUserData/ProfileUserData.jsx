7c025cfe32a41326e3d01bc3372e87455db23166
Dmitriy <dmitriyvolkov002@gmail.com>
2024-04-27 12:18:54 +0300

    изменено изображение

---
+++ src/widgets/ProfileUserData/ProfileUserData.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import s from './style.module.css';
+import Avatar from '@mui/material/Avatar';
+import { Button } from '@mui/material';
+
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import SensorOccupiedIcon from '@mui/icons-material/SensorOccupied';
+
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { useNavigate } from 'react-router-dom';
+import { REQUESTS_PAGE } from '@utils/constants/routes';
+
+export default function ProfileUserData({ user }) {
+    const navigate = useNavigate();
+    return (
+        <div className={s.wrapper}>
+            <Avatar className={s.avatar}>
+                ИС
+            </Avatar>
+            
+            <div className={s.dataSection}>
+                <div className={s.userBlock}>
+                    <div className={s.userMainData}>
+                        <h2 className={s.userName + ' ' + s.title}>
+                            {user.name}
+                        </h2>
+                        <p className={s.userEmail}>
+                            {user.email}
+                        </p>
+                    </div>
+                    
+                    <div className={s.informBlock}>
+                        <div className={s.informItem}>
+                            <PeopleAltIcon/>
+                            <p>Отдел: <span>{user.division_name}</span></p>
+                        </div>
+                        <div className={s.informItem}>
+                            <SensorOccupiedIcon/>
+                            <p>Роль: <span>{user.role_name}</span></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={s.settingsBlock}>
+                    <h2 className={s.title}>Настройки</h2>
+                    <FormControlLabel className={s.settingsForm} control={<Checkbox defaultChecked />} label="Присылать уведомления на почту" />
+                </div>
+
+                <div className={s.rightBlock}>
+                    <Button onClick={() => navigate(REQUESTS_PAGE)} variant='outlined'>К заявкам</Button>
+                    <Button variant='outlined'>Выйти</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
