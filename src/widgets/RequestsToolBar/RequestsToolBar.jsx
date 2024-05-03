da81ad876363a54c1f6b4104fbaaccd85462f78c
Dmitriy <dmitriyvolkov002@gmail.com>
2024-05-03 11:39:37 +0300

    Вынесен в отдельный компонент main toolbar

--- src/widgets/RequestsToolBar/RequestsToolBar.jsx
+++ src/widgets/RequestsToolBar/RequestsToolBar.jsx
@@ -3,17 +3,17 @@ import { Button } from '@mui/material';
 import { CREATE_REQUEST_PAGE } from '@utils/constants/routes';
 import { useNavigate } from 'react-router-dom';
 import RefreshBut from '@components/RefreshBut/RefreshBut';
-import s from './style.module.css';
+import MainToolbar from '../../components/MainToolbar/MainToolbar';
 
 export default function RequestsToolBar({ getAllRequests }) {
     const navigate = useNavigate();
     return (
-        <div className={s.wrapper}>
+        <MainToolbar>
             <Button onClick={() => navigate(CREATE_REQUEST_PAGE)} variant="outlined">Создать заявку</Button>
             <RefreshBut 
                 onClick={getAllRequests}
                 color="primary"    
             />
-        </div>
+        </MainToolbar>
     )
 }
