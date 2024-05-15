495834819c4c99d9d5d35b1f6ae987b568f731a1
Dmitriy <dmitriyvolkov002@gmail.com>
2024-05-15 11:25:20 +0300

    добавлена тема компании

--- src/index.js
+++ src/index.js
@@ -4,15 +4,31 @@ import './index.css';
 import App from './App';
 import UserContextProvider from './contexts/User/UserContextProvider';
 import ActionsContextProvider from './contexts/Actions/ActionsContextProvider';
+import {ThemeProvider} from '@mui/material/styles';
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#f98130',
+      main: '#ff6600',
+      dark: '#cb5303',
+      contrastText: '#fff',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserContextProvider>
-      <ActionsContextProvider>
-        <App />
-      </ActionsContextProvider>
-    </UserContextProvider>
+    <ThemeProvider theme={theme}>
+      <UserContextProvider>
+        <ActionsContextProvider>
+          <App />
+        </ActionsContextProvider>
+      </UserContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
