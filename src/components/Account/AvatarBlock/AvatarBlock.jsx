15205be8cdc24eb5ce2169a23f4cffc753f0396d
Dmitriy <dmitriyvolkov002@gmail.com>
2024-05-15 11:30:25 +0300

    добавлена тема компании

--- src/components/Account/AvatarBlock/AvatarBlock.jsx
+++ src/components/Account/AvatarBlock/AvatarBlock.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import { blue } from '@mui/material/colors';
+import { primary } from '@mui/material/colors';
 import { Box, Tooltip } from '@mui/material';
 
 export default function AvatarBlock({ isOpen, setIsOpen, firstLetters }) {
   return (
     <Box onClick={() => setIsOpen(!isOpen)} sx={{cursor: 'pointer'}}>
         <Tooltip title="Профиль">
-            <Avatar sx={{ bgcolor: blue[400], fontSize: 15}}>
+            <Avatar sx={{ background: '#fff', color: '#ff6600',fontSize: 15}}>
                 {firstLetters}
             </Avatar>
         </Tooltip>
